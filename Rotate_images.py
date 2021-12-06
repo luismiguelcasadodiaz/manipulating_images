862f0a58d3519d029de5906ff0a4f8485204f092
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2021-12-06 21:05:19 +0100

    Added some comments for clarifications

--- Rotate_images.py
+++ Rotate_images.py
@@ -13,19 +13,26 @@ elimiante alpah channel
 import os
 from  PIL import Image as im
 counter=0
-my_directory = os.path.abspath(".")
+my_directory = os.path.abspath(".")  #to know where we are
+
+#path creation to directories with original images and destination
 dir_original_images = os.path.join(my_directory, "images\\original")
 dir_rotated_images = os.path.join(my_directory,"images\\rotated")
+
 print()
 print("I start adjusting Images")
 print("========================")
+
+#for all files listed in the directory of original images
 for f in os.listdir(dir_original_images):
-    if not f.startswith("."):
+    if not f.startswith("."): #except for hidden files
         print(f)
         image_file = os.path.join(dir_original_images,f)
         rotated_image_file = os.path.join(dir_rotated_images,f)
         image_to_rotate = im.open(image_file)
         rotated_image = image_to_rotate.rotate(90)
+        #we need to remove alpha channel of formal "LA" cause it is not
+        #supported by JPEG format
         resized_image = rotated_image.resize((128,128)).convert("L")
         resized_image.save(rotated_image_file,format='PNG')
         counter +=1
